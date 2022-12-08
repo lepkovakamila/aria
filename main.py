339059545c6dc75264bb98065be04fbfdeea384c
Kamila Lepkova <kamila.lepkova@gmail.com>
2022-12-08 16:29:12 -0600

    add class ImageProcessing with functions: get_patients_path, get_flair_path, get_t1_path, create_pat_directory, create_patients_mni_folders, create_patients_work_folders, ants_atlas_registration_t1, ants_atlas_registration_flair

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import nibabel as nib
 import matplotlib.pyplot as plt
 from dicom_to_nifti import dicom_to_nifti_aria
+from data_preprocessing import *
 
 # EDEMA DETECTION AND SEGMENTATION
 
@@ -17,7 +18,44 @@ PATH_DICOM_CONTROL_ARIA_E = '/Users/M217789/PycharmProjects/aria/dataset/control
 #path = ''
 #dicom_to_nifti_aria(path)
 
+# 1.1 Initializing path for media, atlases
+WORKING_DIR = os.getcwd()
+MEDIA_DIR = WORKING_DIR + '/media'
+ATLAS_DIR = MEDIA_DIR + '/atlases'
+JHU_ATLAS_DIR = ATLAS_DIR + '/JHU'
+JHU_ATLAS_TEMPLATE_NII_PATH = JHU_ATLAS_DIR + '/JHU_MNI_SS_template_T1.nii'
+IXI_TEMPLATE_DIR = ATLAS_DIR + '/IXI'
+IXI_TEMPLATE_NII_PATH = IXI_TEMPLATE_DIR + '/T_template0.nii'
+IXI_TEMPLATE_BRAIN_CEREBELLUM_PROBABILITY_MASK_NII_PATH = IXI_TEMPLATE_DIR + \
+                                                          '/T_template_BrainCerebellumProbabilityMask.nii '
+
+pat_directory = os.path.join(WORKING_DIR, 'pat_directory')
+directory_path = '/Users/M217789/PycharmProjects/aria/dataset/aria_data_e'
+EXCEL_INFO = 'aria_e_info.xlsx'
+
+
 # 2. DATA PRE-PROCESSING
+image_preprocessing = ImagePreprocessing(directory_path, WORKING_DIR, pat_directory, JHU_ATLAS_TEMPLATE_NII_PATH, EXCEL_INFO)
+patients_path = image_preprocessing.get_patients_path()
+image_preprocessing.create_pat_directory()
+image_preprocessing.create_patients_mni_folders()
+image_preprocessing.create_patients_work_folders()
+
+flair_paths = image_preprocessing.get_flair_path()
+t1_paths = image_preprocessing.get_t1_path()
+
+for path in t1_paths:
+    image_preprocessing.ants_atlas_registration_t1(path)
+
+for path in flair_paths:
+    image_preprocessing.ants_atlas_registration_flair(path)
+
+
+
+
+stop = 1
+
+
 PATH_ARIA_E_001_DWI_NIFTI = '/dataset/aria_e_data/001/NIFTI/001_DWI.nii'
 PATH_ARIA_E_001_FLAIR_NIFTI = '/Users/M217789/PycharmProjects/mri_pet_amyloid/dataset/aria_e_data/001/FLAIR/NIFTI/001_FLAIR.nii'
 PATH_ARIA_E_001_T1_NIFTI = '/Users/M217789/PycharmProjects/mri_pet_amyloid/dataset/aria_e_data/001/T1/NIFTI/001_T1.nii'
